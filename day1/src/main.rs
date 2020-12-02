265d65d4f8b5abd78d76f19d5c1042927f2357e4
Waldemar <waldemar.schwan@uswitch.com>
2020-12-02 01:17:19 +0000

    task comments

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,5 +1,9 @@
 use std::fs;
-
+// Before you leave, the Elves in accounting just need you to fix your expense report
+// (your puzzle input); apparently, something isn't quite adding up.
+//
+// Specifically, they need you to find the two entries that sum to 2020 and then
+// multiply those two numbers together.
 fn main() {
     let input = fs::read_to_string("input").unwrap();
     let lines = input.lines();
@@ -9,6 +13,9 @@ fn main() {
     println!("bye");
 }
 
+//The Elves in accounting are thankful for your help; one of them even offers you
+//a starfish coin they had left over from a past vacation. They offer you a second one
+//if you can find three numbers in your expense report that meet the same criteria.
 fn match_two(numbers: &Vec<u32>) {
     println!("match two");
     for i in numbers {
