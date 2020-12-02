2729423eee6d65a22b9696f2cbe1cde85db2a48b
Waldemar <waldemar.schwan@uswitch.com>
2020-12-02 01:14:47 +0000

    finish day 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,7 +1,44 @@
 use std::fs;
 
 fn main() {
-    let input = fs::read_to_string("input");
-    let lines = input.unwrap().lines();
-    // let numbers = lines.map(|&l| l.parse::<u32>().unwrap()).collect();
+    let input = fs::read_to_string("input").unwrap();
+    let lines = input.lines();
+    let numbers: Vec<u32> = lines.map(|l| l.parse::<u32>().unwrap()).collect();
+    match_two(&numbers);
+    match_three(&numbers);
+    println!("bye");
+}
+
+fn match_two(numbers: &Vec<u32>) {
+    println!("match two");
+    for i in numbers {
+        for j in numbers {
+            if i == j {
+                continue;
+            }
+            if i + j == 2020 {
+                println!("i: {} + j: {} -> 2020", i, j);
+                println!("i: {} * j: {} -> {}", i, j, i * j);
+                return;
+            }
+        }
+    }
+}
+
+fn match_three(numbers: &Vec<u32>) {
+    println!("match three");
+    for i in numbers {
+        for j in numbers {
+            for k in numbers {
+                if i == j || j == k || k == i {
+                    continue;
+                }
+                if i + j + k == 2020 {
+                    println!("i: {} + j: {} + k:{} -> 2020", i, j, k);
+                    println!("i: {} * j: {} * k:{} -> {}", i, j, k, i * j * k);
+                    return;
+                }
+            }
+        }
+    }
 }
