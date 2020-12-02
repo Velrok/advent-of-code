7fd5094822004d7a8183a52c3ae799e98bffd2e6
Waldemar <waldemar.schwan@uswitch.com>
2020-12-02 01:18:51 +0000

    Update main.rs

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,21 +1,24 @@
 use std::fs;
-// Before you leave, the Elves in accounting just need you to fix your expense report
-// (your puzzle input); apparently, something isn't quite adding up.
-//
-// Specifically, they need you to find the two entries that sum to 2020 and then
-// multiply those two numbers together.
 fn main() {
     let input = fs::read_to_string("input").unwrap();
     let lines = input.lines();
     let numbers: Vec<u32> = lines.map(|l| l.parse::<u32>().unwrap()).collect();
+
+    // Before you leave, the Elves in accounting just need you to fix your expense report
+    // (your puzzle input); apparently, something isn't quite adding up.
+    //
+    // Specifically, they need you to find the two entries that sum to 2020 and then
+    // multiply those two numbers together.
     match_two(&numbers);
+
+    //The Elves in accounting are thankful for your help; one of them even offers you
+    //a starfish coin they had left over from a past vacation. They offer you a second one
+    //if you can find three numbers in your expense report that meet the same criteria.
     match_three(&numbers);
+
     println!("bye");
 }
 
-//The Elves in accounting are thankful for your help; one of them even offers you
-//a starfish coin they had left over from a past vacation. They offer you a second one
-//if you can find three numbers in your expense report that meet the same criteria.
 fn match_two(numbers: &Vec<u32>) {
     println!("match two");
     for i in numbers {
