859d09db5173e32ec818b25e48d5a427b8420021
Waldemar <waldemar.schwan@uswitch.com>
2020-12-13 23:27:50 +0000

    plan day 9

---
+++ day9/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    // given a seq of numbers
+    // construct a moving window of 25 numbers
+    // echo following number must be the sum of any two numbers in the window
+    // first number which is not such a sum is the result
+    //
+    // will need a fn to generat all possible permutations of 25 n in the window
+    //
+    //
+    // read & parse all lines into a Vec numbers
+    // for n in 25..numbers.len()
+    // make a slice of n-25..n
+    // make all possible permuations
+    // summ all tuples into a vec -> check_sum
+    // check that check_sum contains numbers[n]
+    // if not print && exit
+}
