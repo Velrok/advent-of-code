d849df7e6d2f03423f5e7d0a4d29bac42e10da31
Waldemar <waldemar.schwan@uswitch.com>
2020-12-15 20:04:16 +0000

    day 9 part 2 -> Done :D

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 fn parse_numbers(number_lines: &str) -> Vec<usize> {
     number_lines
         .lines()
@@ -29,8 +31,7 @@ fn test_possible_sums() {
     assert_eq!(vec![2, 3, 4, 3, 4, 5, 4, 5, 6], possible_sums(&[1, 2, 3]))
 }
 
-fn part1(lines: &str, preamble: usize) -> usize {
-    let numbers = parse_numbers(lines);
+fn part1(numbers: &Vec<usize>, preamble: usize) -> usize {
     // println!("numbers: {:?}", numbers);
     for i in preamble..numbers.len() {
         let start = i - preamble;
@@ -69,7 +70,25 @@ fn p1_test() {
 309
 576";
     let expected = 127;
-    assert_eq!(expected, part1(input, 5));
+    assert_eq!(expected, part1(&parse_numbers(input), 5));
+}
+
+fn part2(numbers: &Vec<usize>, target: usize) -> usize {
+    for i in 1..numbers.len() {
+        // start at 1 because we need at least 2 numbers
+        for window_size in 2..(i + 1) {
+            let start = i + 1;
+            let window = &numbers[(start - window_size)..start];
+            match window.iter().sum::<usize>().cmp(&target) {
+                Ordering::Less => continue,
+                Ordering::Equal => {
+                    return window.iter().min().unwrap() + window.iter().max().unwrap()
+                }
+                Ordering::Greater => break,
+            }
+        }
+    }
+    0 // ideally we should return a Result :)
 }
 
 fn main() {
@@ -89,5 +108,9 @@ fn main() {
     // check that check_sum contains numbers[n]
     // if not print && exit
     let input = include_str!("../input");
-    println!("part 1 -> {}", part1(input, 25));
+    let numbers = parse_numbers(input);
+
+    println!("part 1 -> {}", part1(&numbers, 25)); // -> 144381670
+
+    println!("part 2 -> {}", part2(&numbers, 144381670)); // -> 144381670
 }
