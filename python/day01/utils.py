ac5c1b2dad52e85a3fa865ca8e4ab4a58c2805fb
Waldemar <waldemar.schwan@uswitch.com>
2021-02-11 22:55:36 +0000

    refactor and generalise

--- python/day01/utils.py
+++ python/day01/utils.py
@@ -1,2 +1,6 @@
+def read_from(filename, line_fn=str):
+    return [line_fn(l) for l in open(filename, 'r').readlines()]
+
+
 def read_numbers_from(filename):
-    return [int(l) for l in open(filename, 'r').readlines()]
+    return read_from(filename, int)
