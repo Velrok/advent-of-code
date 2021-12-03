c400532ce00d4ac7ef25b085210e1ca32127f354
Waldemar (velrok) <34974+Velrok@users.noreply.github.com>
2021-12-03 23:08:49 +0000

    day 3 p2 DONe

--- 2021/advent/src/advent/day_3.clj
+++ 2021/advent/src/advent/day_3.clj
@@ -19,6 +19,7 @@
   (Long/parseLong (cond
                     (string? s) s
                     (seq? s) (reduce str s)
+                    (vector? s) (reduce str s)
                     :else (throw (ex-info "unsupported type" {:type (type s)})))
                   2))
 
@@ -32,6 +33,37 @@
     (* (parse-binary common)
        (parse-binary uncommon))))
 
+(defn filter-rows
+  [matrix criteria]
+  (loop [m       matrix
+         col-idx 0]
+    (if (= 1 (count m))
+      ;; return the only row left
+      (first m)
+      ;;
+      (let [row-count (count m)
+            sum (reduce + (col m col-idx))
+            look-for (criteria row-count sum)]
+        (recur (filter (fn [row] (= look-for (nth row col-idx)))
+                       m)
+               (inc col-idx))))))
+
+(defn oxygen-rating
+  [matrix]
+  (filter-rows matrix
+               (fn [row-count sum]
+                 (if (<= (/ row-count 2) sum)
+                   1
+                   0))))
+
+(defn scrubber-rating
+  [matrix]
+  (filter-rows matrix
+               (fn [row-count sum]
+                 (if (<= (/ row-count 2) sum)
+                   0
+                   1))))
+
 (comment
 
   (first (read-bit-matrix "inputs/day3.test"))
@@ -42,8 +74,24 @@
           power-level))
 
   (->> "inputs/day3.p1"
-       read-strings
+       read-bit-matrix
        power-level)
+
+  (->> "inputs/day3.test"
+       read-bit-matrix
+       oxygen-rating)
+
+  (->> "inputs/day3.test"
+       read-bit-matrix
+       scrubber-rating
+       parse-binary)
+
+;; P2
+
+  (let [matrix (->> "inputs/day3.p2"
+                    read-bit-matrix)]
+    (* (-> matrix scrubber-rating parse-binary)
+       (-> matrix oxygen-rating parse-binary)))
 
   ;;
   )
