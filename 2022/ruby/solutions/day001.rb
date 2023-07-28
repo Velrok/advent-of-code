d7f5c99fc28ca1f696be8a0113ded174b4b88c93
Waldemar <34974+Velrok@users.noreply.github.com>
2023-07-29 00:04:10 +0100

    condense code

--- 2022/ruby/solutions/day001.rb
+++ 2022/ruby/solutions/day001.rb
@@ -8,15 +8,11 @@ extend T::Sig # rubocop:disable Style/MixinUsage
 
 sig { params(file_name: String).void }
 def main(file_name)
-  lines = File.readlines(file_name)
-  number_groups = line_groups(lines).map do |group|
-    group.map(&:to_i)
-  end
-  group_sums = number_groups.map do |group|
-    group.sum(0)
-  end
-  # puts group_sums.max # part 1
-  puts group_sums.sort.reverse.take(3).sum(0)
+  elfs = line_groups(File.readlines(file_name))
+         .map { |group| group.map(&:to_i) }
+         .map { |group| group.sum(0) }
+  # puts elfs.max # part 1
+  puts elfs.sort.reverse.take(3).sum(0)
 end
 
 main('problem-inputs/day001.input')
