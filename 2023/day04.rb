4b0b749c66160b29e7f4155ecf742c4734abbe0b
Waldemar <34974+Velrok@users.noreply.github.com>
2023-12-05 23:00:10 +0000

    day 4 part2

--- 2023/day04.rb
+++ 2023/day04.rb
@@ -27,11 +27,15 @@ class Card < T::Struct
 
   sig { returns(Numeric) }
   def points
-    matches = drawn
+    score > 0 ? 2**(score-1) : 0
+  end
+
+
+  sig { returns(Numeric) }
+  def score
+    @score ||= drawn
       .select { winning.include?(_1) }
       .length
-
-    matches > 0 ? 2**(matches-1) : 0
   end
 end
 
@@ -46,6 +50,36 @@ def part1(cards)
     .sum
 end
 
+sig { params(cards: T::Array[Card]).returns(Numeric) }
+def part2(cards)
+  card_repo = cards.group_by(&:card_no)
+  backlog = cards.clone # we start with just the first
+  total_score = 0 # keep track of the total of cards we looked at
+
+  while card = backlog.shift # take out the first card
+    total_score += 1
+    score = card.score # score is how many winning numbers match
+    next if score == 0 # this means we win no extra cards for this one, so we move on
+
+    # we will add score no of cards so we can add them to the total now
+    # total_score += score
+
+    # if we score one we look one more card ahead 1..1
+    card_numbers = (1..score).map { |offset| card.card_no + offset }
+
+    # puts "card: #{card.card_no} score: #{score} adds: #{card_numbers}"
+
+    card_numbers.each do |card_no|
+      # lookup the winner and add it to the backlog
+      backlog << card_repo[card_no].first
+    end
+    # repeat until we run out of cards to look at
+  end
+
+  total_score
+end
+
+
 puts "--- Day 4 ---"
-pp part1(cards)
+# pp part2(cards)
 puts "-------------"
