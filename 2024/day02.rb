03ca4518c3a3a6f20086433a22fbab1f084b34a7
Waldemar <34974+Velrok@users.noreply.github.com>
2025-01-12 22:12:15 +0000

    2024: day02 p1 in ruby again (now with objects)

--- 2024/day02.rb
+++ 2024/day02.rb
@@ -1,46 +1,93 @@
 # frozen_string_literal: true
 # typed: strong
 
-input = File.readlines('day02.example').map do |line|
-  line.strip.split(' ').map(&:to_i)
-end
+require 'pp'
 
-def safe?(report)
-  first, second = report
+# input = File.readlines('day02.example').map do |line|
+#   line.strip.split(' ').map(&:to_i)
+# end
+#
+# def safe?(report)
+#   first, second = report
+#
+#   direction = case first <=> second
+#   when 0
+#     return false
+#   when -1
+#     :growing
+#   when 1
+#     :shrinking
+#   end
+#
+#   x = report.map.with_index do |this_datapoint, i|
+#     next_datapoint = report[i + 1]
+#
+#     # reached the end
+#     if next_datapoint.nil?
+#       true
+#     else
+#       diff = next_datapoint - this_datapoint
+#
+#       case direction
+#       when :shrinking
+#         [-1,-2,-3].include?(diff)
+#       when :growing
+#         [1,2,3].include?(diff)
+#       end
+#     end
+#   end
+#
+#   x.all?
+# end
+#
+# safe_reports = input.select do |report|
+#   puts "Checking #{report.inspect}"
+#   safe?(report)
+# end
+#
+# puts safe_reports.count
 
-  direction = case first <=> second
-  when 0
-    return false
-  when -1
-    :growing
-  when 1
-    :shrinking
+class Report
+
+  def self.from_line(line)
+    numbers = line.strip.split(' ').map(&:to_i)
+    Report.new(numbers)
   end
 
-  x = report.map.with_index do |this_datapoint, i|
-    next_datapoint = report[i + 1]
+  def initialize(numbers)
+    @numbers = numbers
+  end
 
-    # reached the end
-    if next_datapoint.nil?
-      true
+  def safe?
+    if increasing?
+      @numbers.each_cons(2).all? do |a, b|
+        b >= a + 1 && b <= a + 3
+      end
     else
-      diff = next_datapoint - this_datapoint
-
-      case direction
-      when :shrinking
-        [-1,-2,-3].include?(diff)
-      when :growing
-        [1,2,3].include?(diff)
+      @numbers.each_cons(2).all? do |a, b|
+        b <= a - 1 && b >= a - 3
       end
     end
   end
 
-  x.all?
+  def increasing?
+    @numbers.first < @numbers[1]
+  end
+
 end
 
-safe_reports = input.select do |report|
-  puts "Checking #{report.inspect}"
-  safe?(report)
+def part01
+  reports = File.readlines('day02.input').map do |line|
+    Report.from_line(line)
+  end
+
+  # reports.each do |report|
+  #   pp report
+  #   pp report.safe?
+  # end
+
+  pp reports.filter(&:safe?)
+    .size()
 end
 
-puts safe_reports.count
+part01
