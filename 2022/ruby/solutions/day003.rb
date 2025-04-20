e4da4ea7debf3a8a7bcd7b0ca141ff26296693f2
Waldemar <34974+Velrok@users.noreply.github.com>
2025-04-20 22:29:33 +0100

    2022: day 03 P2

--- 2022/ruby/solutions/day003.rb
+++ 2022/ruby/solutions/day003.rb
@@ -7,7 +7,7 @@ require './utils'
 extend T::Sig # rubocop:disable Style/MixinUsage
 
 sig { params(file_name: String).void }
-def part1(file_name)
+def part1(file_name) # rubocop:disable Metrics/MethodLength
   priorities = []
   File.readlines(file_name).each do |line|
     puts "line: #{line}"
@@ -67,5 +67,39 @@ def priority(dup)
   T.must(priority)
 end
 
+sig { params(file_name: String).void }
+def part2(file_name)
+  line_groups = File
+                .readlines(file_name)
+                .map { _1.chomp }
+                .map { _1.split('') }
+                .map(&:to_set)
+                .each_slice(3)
+  result = line_groups.map do |group|
+    puts "group: #{group.inspect}"
+    common_one = common_character(group)
+    puts "common_one: #{common_one.inspect}"
+    puts ''
+    priority(common_one)
+  end.sum
+
+  puts "result: #{result}"
+end
+
+sig { params(sets: T::Array[T::Set[String]]).returns(String) }
+def common_character(sets)
+  s1 = T.must(sets[0])
+  s2 = T.must(sets[1])
+  s3 = T.must(sets[2])
+
+  common_between_all = s1 & s2 & s3
+
+  raise "found more than 1 common charater: #{common_between_all}" unless common_between_all.length == 1
+
+  T.must(common_between_all.first)
+end
+
 # main('problem-inputs/day003.example')
-part1('problem-inputs/day003.input')
+# part1('problem-inputs/day003.input')
+# part2('problem-inputs/day003.example')
+part2('problem-inputs/day003.input')
