189ec0231d044651280ec34d976e16a9586c6a05
Waldemar Schwan <waldemar@Waldemars-MacBook-Air.local>
2025-08-03 19:21:59 +0100

    2017: extract util

--- 2017/src/day01.zig
+++ 2017/src/day01.zig
@@ -8,7 +8,7 @@ pub fn main() !void {
 
     const content = try file.readToEndAlloc(allocator, 1024 * 10);
 
-    const numbers = try parseInput(allocator, std.mem.trim(u8, content, " \n"));
+    const numbers = try utils.parseDigits(allocator, std.mem.trim(u8, content, " \n"));
     print("part 1: {any}\n", .{part01(
         numbers.items,
     )});
@@ -43,41 +43,7 @@ test "part01 examples" {
     try testing.expectEqual(9, part01(&.{ 9, 1, 2, 1, 2, 1, 2, 9 }));
 }
 
-const ParseErrors = error{NaN};
-
-/// Parses string char by char. Returns an ArrayList the consumer owns and needs to deinit.
-/// Ignores new lines.
-fn parseInput(allocator: std.mem.Allocator, input: []const u8) !std.ArrayList(u8) {
-    var result = std.ArrayList(u8).init(allocator);
-    errdefer result.deinit();
-
-    for (input) |c| {
-        const valid = switch (c) {
-            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' => true,
-            else => false,
-        };
-        if (valid) {
-            try result.append(c - '0');
-        } else {
-            print("{any} not a number\n", .{c});
-            return ParseErrors.NaN;
-        }
-    }
-    return result;
-}
-
-test parseInput {
-    const result = try parseInput(std.testing.allocator, "1234567890");
-    defer result.deinit();
-    const expected: []const u8 = &.{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
-    try testing.expectEqualSlices(u8, expected, result.items);
-}
-
-test "parseInput errors if string contains none digits" {
-    const result = parseInput(std.testing.allocator, "123A4567890");
-    try testing.expectError(ParseErrors.NaN, result);
-}
-
+const utils = @import("utils.zig");
 const std = @import("std");
 const print = std.debug.print;
 const testing = std.testing;
---
+++ 2017/src/utils.zig
@@ -0,0 +1,37 @@
+const ParseErrors = error{NaN};
+
+/// Parses string char by char. Returns an ArrayList the consumer owns and needs to deinit.
+pub fn parseDigits(allocator: std.mem.Allocator, input: []const u8) !std.ArrayList(u8) {
+    var result = std.ArrayList(u8).init(allocator);
+    errdefer result.deinit();
+
+    for (input) |c| {
+        const valid = switch (c) {
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' => true,
+            else => false,
+        };
+        if (valid) {
+            try result.append(c - '0');
+        } else {
+            print("{any} not a number\n", .{c});
+            return ParseErrors.NaN;
+        }
+    }
+    return result;
+}
+
+test parseDigits {
+    const result = try parseDigits(std.testing.allocator, "1234567890");
+    defer result.deinit();
+    const expected: []const u8 = &.{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+    try testing.expectEqualSlices(u8, expected, result.items);
+}
+
+test "parseDigits errors if string contains none digits" {
+    const result = parseDigits(std.testing.allocator, "123A4567890");
+    try testing.expectError(ParseErrors.NaN, result);
+}
+
+const std = @import("std");
+const print = std.debug.print;
+const testing = std.testing;
