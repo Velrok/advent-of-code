fbe137e50cc8fa98ae8c5f89b7481e39702ad242
Waldemar <34974+Velrok@users.noreply.github.com>
2025-09-19 23:40:26 +0100

    2018: Day 01 P2 :done:

--- 2018/Sources/day01/main.swift
+++ 2018/Sources/day01/main.swift
@@ -12,6 +12,33 @@ func part01() -> Int {
         .map { Int($0)! }
         .reduce(0, +)
 }
-func part02() {}
 
-print(part01())
+enum ProcessError: Error {
+    case SearchExhausted
+}
+
+func part02() throws -> Int {
+    let input = try! String(contentsOf: URL(fileURLWithPath: "input/day01"))
+    let frequencyAdjustments =
+        input
+        .trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        .components(separatedBy: CharacterSet.newlines)
+        .map { Int($0)! }
+
+    var frequency = 0
+    var valuesSeen = Set<Int>()
+    for i in 0...Int.max {
+        let adjustment = frequencyAdjustments[i % frequencyAdjustments.count]
+        let newFreq = frequency + adjustment
+        if valuesSeen.contains(newFreq) {
+            return newFreq
+        } else {
+            valuesSeen.update(with: newFreq)
+            frequency = newFreq
+        }
+    }
+    throw ProcessError.SearchExhausted
+}
+
+// print(part01())
+try! print(part02())
