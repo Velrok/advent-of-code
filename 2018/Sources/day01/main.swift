3fb1ac3bcf5763e38d011af834eb8cc30e77f916
Waldemar <34974+Velrok@users.noreply.github.com>
2025-09-19 23:14:33 +0100

    2018: day01 P1 (swift)

--- 2018/Sources/day01/main.swift
+++ 2018/Sources/day01/main.swift
@@ -1,8 +1,16 @@
 import Foundation
 
-func part01() {
-    let example = String(contentsOf: URL())
+func part01() -> Int {
     print("day01")
+
+    let input = try! String(contentsOf: URL(fileURLWithPath: "input/day01"))
+
+    return
+        input
+        .trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        .components(separatedBy: CharacterSet.newlines)
+        .map { Int($0)! }
+        .reduce(0, +)
 }
 func part02() {}
 
