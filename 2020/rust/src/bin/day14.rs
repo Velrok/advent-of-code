c264248e5ffa0a2fadae5de3bbc5458c8b11539c
Waldemar <34974+Velrok@users.noreply.github.com>
2025-11-21 22:07:57 +0000

    2020: learning rusy again this time more slowly

---
+++ 2020/rust/src/bin/day14.rs
@@ -0,0 +1,23 @@
+enum Instruction {
+    SetMask(),
+    SetMemory(),
+}
+
+fn parse_line(line: &str) -> Result<Instruction, String> {
+    if let Some(_mask) = line.strip_prefix("mask = ") {
+        Ok(Instruction::SetMask())
+    } else if let Some(_mem_data) = line.strip_prefix("mem") {
+        Ok(Instruction::SetMemory())
+    } else {
+        Err(format!("Cant parse line: {line}"))
+    }
+}
+
+fn main() -> () {
+    let input = std::fs::read_to_string("./inputs/day14.txt")
+        .expect("Failed to read ./inputs/day14.txt")
+        .lines()
+        .map(parse_line);
+
+    println!("Hello, world!");
+}
