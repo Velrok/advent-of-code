f88466b0715044adbc421b016541141d00e6336b
Waldemar <34974+Velrok@users.noreply.github.com>
2025-12-01 14:47:38 +0000

    2025: D1 P1 (rust)

--- 2025/aoc_rust/src/bin/day01.rs
+++ 2025/aoc_rust/src/bin/day01.rs
@@ -1,14 +1,40 @@
 use std::fs;
 
+type DialCount = i16;
+const DIAL_SIZE: DialCount = 100;
+const DIAL_START: DialCount = 50;
+
 fn main() {
     let input = fs::read_to_string("inputs/day01.txt").unwrap();
-    let _lines = input.lines();
+    let lines = input.lines();
 
-    println!("Day 1 - Advent of Code 2025");
+    let mut dial_pos = DIAL_START;
+    let mut password = 0;
 
-    // Part 1
+    for line in lines {
+        let mut chars = line.chars();
+        let direction = chars.next();
+        let rotation = chars.collect::<String>().parse::<DialCount>().unwrap();
 
-    // Part 2
+        let new_pos = match direction {
+            Some('L') => turn_left(dial_pos, rotation),
+            Some('R') => turn_right(dial_pos, rotation),
+            _ => panic!("unexpected direction"),
+        };
 
-    println!("Done!");
+        if new_pos == 0 {
+            password += 1
+        }
+
+        dial_pos = new_pos;
+    }
+    print!("{password}")
+}
+
+fn turn_left(current: DialCount, rotation: DialCount) -> DialCount {
+    (current - rotation) % DIAL_SIZE
+}
+
+fn turn_right(current: DialCount, rotation: DialCount) -> DialCount {
+    (current + rotation) % DIAL_SIZE
 }
