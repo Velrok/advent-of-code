76f9274eac32c06c75daac6cd517d62e13bcd658
Waldemar <34974+Velrok@users.noreply.github.com>
2025-12-01 12:11:33 +0000

    2025: setup rust

---
+++ 2025/aoc_rust/src/bin/day01.rs
@@ -0,0 +1,14 @@
+use std::fs;
+
+fn main() {
+    let input = fs::read_to_string("inputs/day01.txt").unwrap();
+    let _lines = input.lines();
+
+    println!("Day 1 - Advent of Code 2025");
+
+    // Part 1
+
+    // Part 2
+
+    println!("Done!");
+}
