b4dc10cb3116bdccac7051cccbcee6671ad4687d
Waldemar <34974+Velrok@users.noreply.github.com>
2026-02-15 17:10:15 +0000

    2020 rust: day15.rs part 2 (now fast)

--- 2020/rust/src/bin/day15.rs
+++ 2020/rust/src/bin/day15.rs
@@ -1,34 +1,35 @@
+use im::HashMap;
+
 // --- Day 15: Rambunctious Recitation ---
 fn main() {
     let example_numbers = vec![0, 3, 6];
     let puzzle_numbers = vec![9, 6, 0, 10, 18, 2, 1];
     let mut numbers = puzzle_numbers.clone();
     // let mut numbers = example_numbers.clone();
+    let mut recitation: HashMap<usize, usize> = HashMap::new();
 
-    // 0, 3, 6
-    // 0, 1, 2
-    //        , 3
-    for i in numbers.len()..2020 {
-        println!("Turn: {}", i + 1);
-        let needle = numbers[i - 1];
-        println!(" needle: {needle}");
-        let last_occurences: Vec<_> = numbers
-            .iter()
-            .enumerate() // get a hold of the original indecies as well
-            .rev() // we are going backwards
-            .filter(|(_, x)| **x == needle)
-            .take(2)
-            .map(|(index, _)| index)
-            .collect::<Vec<_>>();
-        println!(" last_occurences: {:?}", last_occurences.as_slice());
-        let age = match last_occurences.as_slice() {
-            [] => 0,
-            [_] => 0,
-            [b, a] => b - a,
-            _ => unreachable!(), // take 2 means no more than 2
+    // let end = 2020;
+    let end = 30000000;
+    for i in 1..end {
+        print!(".");
+        // println!("i: {i}");
+        let needle_index = i - 1;
+        let needle = numbers[needle_index];
+        // println!(" needle: {needle}");
+        let previous_occurence = recitation.get(&needle);
+        let age = match previous_occurence {
+            Some(previous_index) => needle_index - previous_index,
+            None => 0,
         };
-        println!(" age: {age}");
-        numbers.push(age);
+        // println!(" age: {age}");
+
+        match numbers.get(i) {
+            Some(_) => (), // skipp
+            None => numbers.push(age),
+        }
+        // println!(" numbers: {:?}", numbers);
+
+        recitation.insert(needle, needle_index);
     }
 
     if numbers == example_numbers {
