7b2ab652587898fc5c82498df6276fc263a61859
Waldemar <34974+Velrok@users.noreply.github.com>
2026-09-20 17:14:47 +0100

    wip

--- 2019/rust/src/bin/int_vm.rs
+++ 2019/rust/src/bin/int_vm.rs
@@ -1,6 +1,10 @@
 use anyhow::Result;
 use aoc19::intcode::Program;
-use std::io::Read;
+use std::{
+    fs::File,
+    io::{BufReader, BufWriter, Read, Write},
+    path::Path,
+};
 
 enum Commands {
     Run,
@@ -27,29 +31,41 @@ fn main() -> Result<()> {
 }
 
 fn decompile(cmd_args: &[String]) -> Result<()> {
-    let input_path_str = cmd_args.first();
-    let output_path_str = cmd_args.get(1);
-    if let None = input_path_str {
-        anyhow::bail!("decompile needs a file to read")
-    }
-    let mut writer: Box<dyn std::io::Write> = match output_path_str {
-        Some(out_dest) => {
-            let file = std::fs::File::create(std::path::Path::new(out_dest))
-                .expect("Cant open dest_file for writting!");
-            Box::new(std::io::BufWriter::new(file))
+    match cmd_args.first() {
+        None => anyhow::bail!("decompile needs a file to read"),
+        Some(input_path_str) => {
+            let output_path_str = cmd_args.get(1);
+            let mut writer: Box<dyn Write> = match output_path_str {
+                Some(out_dest) => {
+                    let file = File::create(std::path::Path::new(out_dest))
+                        .expect("Cant open dest_file for writting!");
+                    Box::new(BufWriter::new(file))
+                }
+                None => Box::new(std::io::stdout()),
+            };
+
+            let input_path = Path::new(input_path_str);
+
+            let file = File::open(input_path);
+            let ints = BufReader::new(file?).split(b',').map(|bytes| {
+                str::from_utf8(bytes)
+                    .expect("Expected UTF8 encoding")
+                    .parse::<i64>()
+                    .expect("Expected to only see i64 numbers.");
+            });
+
+            // we can tokenize by ,
+            // then read the int as an op type
+            // should be able to reuse fn read_instruction(&self) -> Instruction after some refactor to take
+            // an opcode: Word and some mem slice *[Word]
+            // now might be the time to give Instruction a width we know how many to read
+            // we can do this in a loop until we get to the end of the tokens
+            // we shoudl end up with a [Instrction]
+            // we can then map a translator Instrction -> String
+            // and finally print to the writer one line per Instruction
+            todo!()
         }
-        None => Box::new(std::io::stdout()),
-    };
-    // we can tokenize by ,
-    // then read the int as an op type
-    // should be able to reuse fn read_instruction(&self) -> Instruction after some refactor to take
-    // an opcode: Word and some mem slice *[Word]
-    // now might be the time to give Instruction a width we know how many to read
-    // we can do this in a loop until we get to the end of the tokens
-    // we shoudl end up with a [Instrction]
-    // we can then map a translator Instrction -> String
-    // and finally print to the writer one line per Instruction
-    todo!()
+    }
 }
 
 fn print_help() {
